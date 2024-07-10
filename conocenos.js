74a98b53b7f284784890f27778df3db20b180a94
Jesus Perafan <jecaperafan@unicauca.edu.co>
2024-07-09 22:29:54 -0500

    Seccion-about
    Se agrega la nueva seccion de la pagina (conocenos), pedir la información para completar con lo que ellos quieran

---
+++ conocenos.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const images = [
+      'url("../IMAGES/parque.JPEG")',
+      'url("../IMAGES/silvia.jpg")',
+      'url("../IMAGES/purace3.jpg")',
+      'url(../IMAGES/tierradentro5.jpg)'
+    ];
+  
+    const gradients = [
+        'linear-gradient(to right, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 255))',
+        'linear-gradient(to right, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 255))',
+        'linear-gradient(to right, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 255))',
+        'linear-gradient(to right, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 255))'
+      ];
+    
+      let currentIndex = 0;
+      const slideshowElement = document.getElementById('agencia');
+      const overlay = document.createElement('div');
+      overlay.className = 'overlay';
+      slideshowElement.appendChild(overlay);
+    
+      function changeBackground() {
+        overlay.style.opacity = 1;
+        setTimeout(() => {
+          slideshowElement.style.backgroundImage = `${gradients[currentIndex]}, ${images[currentIndex]}`;
+          currentIndex = (currentIndex + 1) % images.length;
+          setTimeout(() => {
+            overlay.style.opacity = 0;
+          }, 1000); // Espera el tiempo de la transición antes de ocultar el overlay
+        }, 1000); // Espera el tiempo de la transición antes de cambiar la imagen
+      }
+    
+      // Cambia la imagen de fondo cada 5 segundos
+      setInterval(changeBackground, 7000);
+    
+      // Cambia la imagen inmediatamente al cargar la página
+      changeBackground();
+    });
