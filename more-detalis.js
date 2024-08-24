8933561fb190aa2cfbf35c7ab56df57d4f1a2a90
Jesus Perafan <jecaperafan@unicauca.edu.co>
2024-08-23 23:00:52 -0500

    Ajustes-historias-usuario
    Eliminación de tarjeta religión en index Popayán, y solución de superposición de ventanas en index bloqueando el scroll de la pagina principal al estar abierta la ventana de más detalles

--- more-detalis.js
+++ more-detalis.js
@@ -8,18 +8,23 @@ btns.forEach(function(btn) {
         var modal = document.getElementById(modalId);
         modal.style.display = "block";
 
+        // Desactivar el desplazamiento de la página principal
+        document.body.style.overflow = 'hidden';
+
         // Obtener el <span> que cierra el modal
         var span = modal.getElementsByClassName("close")[0];
 
         // Cuando el usuario haga clic en <span> (x), cierra el modal
         span.onclick = function() {
             modal.style.display = "none";
+            document.body.style.overflow = ''; // Restaurar el desplazamiento
         }
 
         // Cuando el usuario haga clic en cualquier lugar fuera del modal, lo cierra
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
+                document.body.style.overflow = ''; // Restaurar el desplazamiento
             }
         }
     }
